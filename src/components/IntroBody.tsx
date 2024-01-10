38a02b1b77bc0549018b5e93b170eee615838191
Michael Wlodawsky <michaelwlodawsky@gmail.com>
2024-01-10 17:46:36 -0600

    testing NODE_ENV

--- src/components/IntroBody.tsx
+++ src/components/IntroBody.tsx
@@ -12,6 +12,7 @@ export default function IntroBody() {
     
     const searchParams = useSearchParams()
     const token = searchParams.get('token')
+    console.log(process.env.NODE_ENV)
 
     useEffect(() => {
         const timer = setInterval(() => {
