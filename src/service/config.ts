454fe43d2ad7374885088e18eb36ae8b0921f78b
Michael Wlodawsky <michaelwlodawsky@gmail.com>
2024-02-08 09:44:10 -0600

    update api key

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAZArOEaGFKrigURlOPpgG0CnuA4weAOiY",
+    apiKey: "AIzaSyCx_35Le3pbws15Z056JVanN8nFjoPslYI",
     authDomain: "michaelwlodawsky-homepage.firebaseapp.com",
     projectId: "michaelwlodawsky-homepage",
     storageBucket: "michaelwlodawsky-homepage.appspot.com",
     messagingSenderId: "1025690015406",
     appId: "1:1025690015406:web:c07e98e4aee696d1f7bf3f"
-};
+  };
