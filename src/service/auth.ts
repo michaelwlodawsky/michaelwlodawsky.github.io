117dcbf312123803cb8d492267c7325230fff650
Michael Wlodawsky <michaelwlodawsky@gmail.com>
2024-02-08 09:19:32 -0600

    remove API key from public repo, it's been updated

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -1,15 +1,6 @@
 import { UserCredential, getAuth, signInAnonymously } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAZArOEaGFKrigURlOPpgG0CnuA4weAOiY",
-    authDomain: "michaelwlodawsky-homepage.firebaseapp.com",
-    projectId: "michaelwlodawsky-homepage",
-    storageBucket: "michaelwlodawsky-homepage.appspot.com",
-    messagingSenderId: "1025690015406",
-    appId: "1:1025690015406:web:c07e98e4aee696d1f7bf3f",
-    measurementId: "G-G761Q82M2Q"
-};
+import { firebaseConfig } from "./config";
   
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
