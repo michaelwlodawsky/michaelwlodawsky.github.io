df6450b21287fb3aaef151c0f80eb558503f81bf
Michael Wlodawsky <michaelwlodawsky@gmail.com>
2024-02-15 17:03:37 -0600

    just make animation simple for now so I can focus on other projects

--- src/components/HorizontalScrollContainer.tsx
+++ src/components/HorizontalScrollContainer.tsx
@@ -2,63 +2,46 @@
 
 import React from "react";
 import { gsap } from "gsap";
+import style from "../app/page.module.css";
+import { styled, keyframes } from "styled-components";
 
 export default function HorizontalScrollContainer({ text }: { text: string }) {
     const root = React.useRef(null);
-    const [mouseHovering, setMosueHovering] = React.useState(false);
-    let progress: number = 0
+    // const [mouseHovering, setMosueHovering] = React.useState(false);
 
-    React.useEffect(() => {
-        // TODO: Have mouse hover slowdown animation too
-        /*
-            Animation is currently broken: I need to persist values of the animation across re-renders and set the values accordingly
+    // React.useEffect(() => {
+    //     let scrollStarted = false;
+    //     const handleScrollStart = () => {
+    //         if (!scrollStarted) {
+    //             // First scroll start
+    //             // tween.timeScale(0.5);
+    //         }
 
-            start x at 0% and move to -100%, repeat -1 always, duration ??, ease linear always
+    //         scrollStarted = true;
+    //     };
 
-            1. on re-render, pause the animation to get stable values
-            2. save the progress
-            3. set start x to -100 * progress, set duration to 
-        
-        */
-        let tween = gsap.to(".scrollContent", {xPercent: -100, repeat: -1, duration: 15, ease: "linear"});
+    //     const handleScrollEnd = () => {
+    //         // tween.timeScale(1);
+    //         scrollStarted = false;
+    //     };
 
-        let scrollStarted = false;
+    //     window.addEventListener("scroll", handleScrollStart);
+    //     window.addEventListener("scrollend", handleScrollEnd);
 
-        const handleScrollStart = () => {
-            if (!scrollStarted) {
-                // First scroll start
-                tween.timeScale(0.5);
-            }
+    //     return () => {
+    //         // tween.kill();
+    //         window.removeEventListener('scroll', handleScrollStart);
+    //         window.removeEventListener('scrollend', handleScrollEnd);
+    //     };
+    // }, []);
 
-            scrollStarted = true;
-        };
-
-        const handleScrollEnd = () => {
-            tween.timeScale(1);
-            scrollStarted = false;
-        };
-
-        window.addEventListener("scroll", handleScrollStart);
-        window.addEventListener("scrollend", handleScrollEnd);
-
-        // if (mouseHovering) {
-            
-        // } else {
-
-        // }
-
-        return () => {
-            tween.kill();
-            window.removeEventListener('scroll', handleScrollStart);
-            window.removeEventListener('scrollend', handleScrollEnd);
-        };
-    }, []);
+    // onMouseEnter={() => setMosueHovering(true)} onMouseLeave={() => setMosueHovering(false)}
 
     return (
-       <div onMouseEnter={() => setMosueHovering(true)} onMouseLeave={() => setMosueHovering(false)} style={{ width: '100%', overflowX: 'hidden', userSelect: 'none'}} ref={root}>
-            <div style={{whiteSpace: 'nowrap'}}>
-                <p className='scrollContent' style={{display: 'inline-block', minWidth: '100%', whiteSpace: 'pre'}}>{text}</p>
-                <p className='scrollContent' style={{display: 'inline-block', minWidth: '100%', whiteSpace: 'pre'}}>{text}</p>
+       <div style={{ width: '100%', overflowX: 'hidden', userSelect: 'none'}} ref={root}>
+            <div className={style.scrollContainerWrapper}>
+                <p className={style.scrollContainer}>{text}</p>
+                <p className={style.scrollContainer}>{text}</p>
             </div>
        </div> 
     );
